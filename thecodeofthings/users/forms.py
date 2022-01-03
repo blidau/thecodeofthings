b3d277d9fd05b96ff97045c38102f7095660545e
Benjamin Laird <benjamin@bl.id.au>
2022-01-03 15:38:18 +1100

    Update user to add Twitter username and format files

--- thecodeofthings/users/forms.py
+++ thecodeofthings/users/forms.py
@@ -5,7 +5,9 @@ from wagtail.users import forms as wagtail_users_forms
 
 class UserEditForm(wagtail_users_forms.UserEditForm):
     resource = forms.URLField(required=False, label=_("Resource"))
+    twitter_username = forms.CharField(required=False, label=_("Twitter username"))
 
 
 class UserCreationForm(wagtail_users_forms.UserCreationForm):
     resource = forms.URLField(required=False, label=_("Resource"))
+    twitter_username = forms.CharField(required=False, label=_("Twitter username"))
--- thecodeofthings/users/models.py
+++ thecodeofthings/users/models.py
@@ -4,3 +4,4 @@ from django.db import models
 
 class User(auth_models.AbstractUser):
     resource = models.URLField(blank=True)
+    twitter_username = models.CharField(max_length=15, blank=True)

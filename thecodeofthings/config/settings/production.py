235f0e172ad1fc0040d336abd8c3acb6d5292c1c
Benjamin Laird <benjamin@bl.id.au>
2022-01-11 16:59:41 +1100

    Add production settings

--- thecodeofthings/config/settings/production.py
+++ thecodeofthings/config/settings/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["thecodeofthings.com", "www.thecodeofthings.com"]
+
 try:
     from .local import *
 except ImportError:
